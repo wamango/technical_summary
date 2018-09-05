c6b1bf3f7763a22928b9e48c01fc5816b42e8d8f
dzm <374365982@qq.com>
2018-09-05 20:36:11 +0800

    修整

--- src/main/java/com/example/demo/exercise/Test.java
+++ src/main/java/com/example/demo/exercise/Test.java
@@ -1,8 +1,5 @@
 package com.example.demo.exercise;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 测试类
  *
@@ -12,13 +9,6 @@ import java.util.List;
 public class Test {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(3);
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        System.out.println(list);
-
 
     }
 
